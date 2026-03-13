7dc4794b8a6037562db00f21e6c223c873d36e7d
XulbuX <xulbux.real@gmail.com>
2026-03-13 22:42:58 +0100

    linting

--- src/xulbux/color.py
+++ src/xulbux/color.py
@@ -800,10 +800,10 @@ class Color:
                     and all(isinstance(val, int) for val in array_color[:3])
                     and isinstance(array_color[3], (float, type(None)))
                     ):
-                    return (
-                        0 <= array_color[0] <= 255 and 0 <= array_color[1] <= 255 and 0 <= array_color[2] <= 255
-                        and (array_color[3] is None or 0 <= array_color[3] <= 1)
-                    )
+                        return (
+                            0 <= array_color[0] <= 255 and 0 <= array_color[1] <= 255 and 0 <= array_color[2] <= 255
+                            and (array_color[3] is None or 0 <= array_color[3] <= 1)
+                        )
                 elif len(array_color) == 3 and all(isinstance(val, int) for val in array_color):
                     return 0 <= array_color[0] <= 255 and 0 <= array_color[1] <= 255 and 0 <= array_color[2] <= 255
                 else:
@@ -817,10 +817,10 @@ class Color:
                     and all(isinstance(dict_color.get(ch), int) for ch in ("r", "g", "b"))
                     and isinstance(dict_color.get("a", "no alpha"), (float, type(None)))
                     ):
-                    return (
-                        0 <= dict_color["r"] <= 255 and 0 <= dict_color["g"] <= 255 and 0 <= dict_color["b"] <= 255
-                        and (dict_color["a"] is None or 0 <= dict_color["a"] <= 1)
-                    )
+                        return (
+                            0 <= dict_color["r"] <= 255 and 0 <= dict_color["g"] <= 255 and 0 <= dict_color["b"] <= 255
+                            and (dict_color["a"] is None or 0 <= dict_color["a"] <= 1)
+                        )
                 elif len(dict_color) == 3 and all(isinstance(dict_color.get(ch), int) for ch in ("r", "g", "b")):
                     return 0 <= dict_color["r"] <= 255 and 0 <= dict_color["g"] <= 255 and 0 <= dict_color["b"] <= 255
                 else:
@@ -851,10 +851,10 @@ class Color:
                     and all(isinstance(val, int) for val in array_color[:3])
                     and isinstance(array_color[3], (float, type(None)))
                     ):
-                    return (
-                        0 <= array_color[0] <= 360 and 0 <= array_color[1] <= 100 and 0 <= array_color[2] <= 100
-                        and (array_color[3] is None or 0 <= array_color[3] <= 1)
-                    )
+                        return (
+                            0 <= array_color[0] <= 360 and 0 <= array_color[1] <= 100 and 0 <= array_color[2] <= 100
+                            and (array_color[3] is None or 0 <= array_color[3] <= 1)
+                        )
                 elif len(array_color) == 3 and all(isinstance(val, int) for val in array_color):
                     return 0 <= array_color[0] <= 360 and 0 <= array_color[1] <= 100 and 0 <= array_color[2] <= 100
                 else:
@@ -868,10 +868,10 @@ class Color:
                     and all(isinstance(dict_color.get(ch), int) for ch in ("h", "s", "l"))
                     and isinstance(dict_color.get("a", "no alpha"), (float, type(None)))
                     ):
-                    return (
-                        0 <= dict_color["h"] <= 360 and 0 <= dict_color["s"] <= 100 and 0 <= dict_color["l"] <= 100
-                        and (dict_color["a"] is None or 0 <= dict_color["a"] <= 1)
-                    )
+                        return (
+                            0 <= dict_color["h"] <= 360 and 0 <= dict_color["s"] <= 100 and 0 <= dict_color["l"] <= 100
+                            and (dict_color["a"] is None or 0 <= dict_color["a"] <= 1)
+                        )
                 elif len(dict_color) == 3 and all(isinstance(dict_color.get(ch), int) for ch in ("h", "s", "l")):
                     return 0 <= dict_color["h"] <= 360 and 0 <= dict_color["s"] <= 100 and 0 <= dict_color["l"] <= 100
                 else:
